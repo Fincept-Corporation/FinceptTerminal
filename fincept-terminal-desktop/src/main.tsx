5f6ef1f6787bbfb3c9fbd84805df9a633bb70343
tilakpatel22 <mygoalisfix@gmail.com>
2025-09-26 11:36:50 +0530

    code update

--- fincept-terminal-desktop/src/main.tsx
+++ fincept-terminal-desktop/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
-import './app.css' // Make sure this line exists
+import './App.css' // Make sure this line exists
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
